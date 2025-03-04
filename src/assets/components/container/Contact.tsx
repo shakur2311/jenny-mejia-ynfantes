4061c0fc30b2eb4da39a5d6462e2787ea24eb739
luismerino <usrprovwaytech7@visivaedu.com>
2025-03-03 20:14:03 -0500

    funcionar envio correos

--- src/assets/components/container/Contact.tsx
+++ src/assets/components/container/Contact.tsx
@@ -1,6 +1,30 @@
 import Social from './Contact/Social.tsx';
+import { FormEvent, useRef} from 'react';
+import emailjs from '@emailjs/browser';
+
 
 const Contact: React.FC = ()=>{
+    const formRef = useRef<HTMLFormElement>(null);
+    const sendEmail = async (e: FormEvent<HTMLFormElement>): Promise<void>=>{
+        e.preventDefault();
+        const form = formRef.current;
+        if (!form) return; 
+
+        emailjs.sendForm(
+            'service_gwjzr7f',
+            'template_x1dbqna',
+            form,
+            'Id0fdApRV60dgcj9-'
+        )
+        .then((result) => {
+            console.log('Mensaje enviado:', result.text);
+        })
+        .catch((error) => {
+            console.error('Error:', error.text);
+        });
+
+        form.reset(); 
+    }
     return (
         <div className='contact' id="contact">
             <div className="container">
@@ -13,21 +37,21 @@ const Contact: React.FC = ()=>{
                     <Social icono="fa-brands fa-instagram" texto="jennyeii_"></Social>
                     <Social icono="fa-brands fa-linkedin" texto="Jenny Mejia Ynfantes"></Social>
                 </div>
-                <div className="form-container">
+                <form ref={formRef} onSubmit={sendEmail} className="form-container">
                     <div className="form-group">
-                        <label htmlFor="correoInput">Correo</label>
-                        <input type="text" id="correoInput" name="correoInput" />
+                        <input type="text" id="correoInput" name="correoInput" required/>
+                        <label htmlFor="correoInput" id="correoLabel">Correo</label>
                     </div>
                     <div className="form-group">
-                        <label htmlFor="asuntoInput">Asunto</label>
-                        <input type="text" id="asuntoInput" name="asuntoInput" />
+                        <input type="text" id="asuntoInput" name="asuntoInput" required/>
+                        <label htmlFor="asuntoInput" id="asuntoLabel">Asunto</label>
                     </div>
                     <div className="form-group">
-                        <label htmlFor="mensajeInput">Mensaje</label>
-                        <textarea id="mensajeInput" name="mensajeInput"></textarea>
+                        <textarea id="mensajeInput" name="mensajeInput" required></textarea>
+                        <label htmlFor="mensajeInput" id="mensajeLabel">Mensaje</label>
                     </div>
                     <button type="submit" className="btn-send" id="btn-send"><span>Enviar</span></button>
-                </div>
+                </form>
             </div>
         </div>
     )
